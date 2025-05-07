0a804d56c9c825b656923971c3147590209d8765
Archwisp <archwisp@gmail.com>
2025-05-07 14:47:15 -0700

    Added file writing

--- scripts/helpers/go_mod_file.py
+++ scripts/helpers/go_mod_file.py
@@ -8,16 +8,19 @@ class GoModFile:
         self.file_name = file_name
 
     def read(self):
-       self.file_contents = open(self.file_name)
+       file_handle = open(self.file_name, "r")
+       self.file_contents = file_handle.read().split("\n")
+       file_handle.close()
     
     def write(self):
-       file_handle = open(self.file_name, "r")
-       file_handle.write(self.file_contents.join("\n"))
+       file_handle = open(self.file_name, "w")
+       file_handle.write('\n'.join(self.file_contents))
        file_handle.close()
 
     def update_vulns(self, vulns):
         self.read()
         line_number = 0
+        updated = 0
 
         for line in self.file_contents:
             for vuln in vulns:
@@ -25,5 +28,9 @@ class GoModFile:
                     print("Found match for %s on line number %s" % (vuln["lib"], line_number))
                     print("Updating version from %s to %s" % (vuln["vuln_version"], vuln["fix_version"]))
                     line.replace(vuln["vuln_version"], vuln["fix_version"])
+                    updated += 1
 
             line_number += 1
+
+        if updated:
+            self.write()
