7e787f0dfcf19719f9562070702cce19e23ba63e
Archwisp <archwisp@gmail.com>
2025-05-12 20:06:53 -0700

    Fixed tabs/spaces

--- src/main.go
+++ src/main.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"golang.org/x/text/language"
+    "fmt"
+    "golang.org/x/text/language"
 )
 
 func Hello(name string, lang string) (message string, err error) {
-	tag, err := language.Parse(lang)
-	
-	if tag == language.English {
-		message = fmt.Sprintf("Hello, %v. Welcome!", name)
-	} else if tag == language.Spanish {
-		message = fmt.Sprintf("Hola, %v. Bienvenidos!", name)
-	} else if tag == language.Russian {
-		message = fmt.Sprintf("Привет, %v. Добро пожаловать!", name)
-	} else {
-		err = fmt.Errorf("Unsupported language")
-	}
+    tag, err := language.Parse(lang)
+    
+    if tag == language.English {
+        message = fmt.Sprintf("Hello, %v. Welcome!", name)
+    } else if tag == language.Spanish {
+        message = fmt.Sprintf("Hola, %v. Bienvenidos!", name)
+    } else if tag == language.Russian {
+        message = fmt.Sprintf("Привет, %v. Добро пожаловать!", name)
+    } else {
+        err = fmt.Errorf("Unsupported language")
+    }
 
-	return message, err
+    return message, err
 }
 
 func main() {
-	message, err := Hello("Bob", "en")
+    message, err := Hello("Bob", "en")
     
-	if err != nil {
-		fmt.Printf("Error occurred: %s\n", err)
-	} else {
-		fmt.Println(message)
-	}
-	
-	message, err = Hello("Bob", "es")
+    if err != nil {
+        fmt.Printf("Error occurred: %s\n", err)
+    } else {
+        fmt.Println(message)
+    }
     
-	if err != nil {
-		fmt.Printf("Error occurred: %s\n", err)
-	} else {
-		fmt.Println(message)
-	}
-	
-	message, err = Hello("Bob", "ru")
+    message, err = Hello("Bob", "es")
     
-	if err != nil {
-		fmt.Printf("Error occurred: %s\n", err)
-	} else {
-		fmt.Println(message)
-	}
+    if err != nil {
+        fmt.Printf("Error occurred: %s\n", err)
+    } else {
+        fmt.Println(message)
+    }
+    
+    message, err = Hello("Bob", "ru")
+    
+    if err != nil {
+        fmt.Printf("Error occurred: %s\n", err)
+    } else {
+        fmt.Println(message)
+    }
 }
--- src/main_test.go
+++ src/main_test.go
@@ -7,21 +7,21 @@ import (
 func TestHello(t *testing.T) {
     name := "Alice"
 
-	expected := "Hello, Alice. Welcome!"
+    expected := "Hello, Alice. Welcome!"
     msg, err := Hello(name, "en")
 
     if msg != expected || err != nil {
         t.Errorf(`Hello("Alice", "en") = %q, %v, want match for %#q, nil`, msg, err, expected)
     }
-	
-	expected = "Hola, Alice. Bienvenidos!"
+    
+    expected = "Hola, Alice. Bienvenidos!"
     msg, err = Hello(name, "es")
 
     if msg != expected || err != nil {
         t.Errorf(`Hello("Alice", "es") = %q, %v, want match for %#q, nil`, msg, err, expected)
     }
-	
-	expected = "Привет, Alice. Добро пожаловать!"
+    
+    expected = "Привет, Alice. Добро пожаловать!"
     msg, err = Hello(name, "ru")
 
     if msg != expected || err != nil {
